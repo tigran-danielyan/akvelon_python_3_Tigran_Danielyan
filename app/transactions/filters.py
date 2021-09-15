7ab7ee96e912aac9d2d38721342c584a428a5ac4
Tigran <tigran@Tigrans-MacBook-Air.local>
2021-09-16 03:54:08 +0400

    update schema.yml

--- app/transactions/filters.py
+++ app/transactions/filters.py
@@ -3,10 +3,9 @@ from transactions.models import Transaction
 
 
 class UserTransactionFilter(filters.FilterSet):
-    transaction_id = filters.CharFilter(lookup_expr='iexact', field_name="id")
     method = filters.NumberFilter(lookup_expr='iexact')
     date = filters.DateFromToRangeFilter(field_name="date")
 
     class Meta:
         model = Transaction
-        fields = ['transaction_id', 'date', 'method']
+        fields = ['date', 'method']
